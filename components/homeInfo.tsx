c6819b7f1cf8836085870ab03608dea51ded1329
Anas sarkiz <anassoftwaredev@gmail.com>
2025-02-22 18:45:49 +0200

    remove tailwind plugins

--- components/homeInfo.tsx
+++ components/homeInfo.tsx
@@ -3,7 +3,7 @@ export function HomeInfo() {
     <div className="flex justify-center items-center w-full">
       <div className="flex flex-col justify-center items-center text-center">
         <h1 className="text-xl md:text-4xl font-bold mb-8">
-          Welcome to <span className="italic">{process.env.SITE_NAME}</span>{" "
+          Welcome to <span className="italic">{process.env.SITE_NAME}</span>{" "}
           website
         </h1>
       </div>
