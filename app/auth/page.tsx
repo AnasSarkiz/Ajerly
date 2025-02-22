10129e6365796f34887655fdc12bcdb34ff6dafc
Shibo <shibosoftwaredev@gmail.com>
2025-02-22 21:46:04 +0200

    fixed build error when using useSearchParams

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,12 +3,21 @@
 import SignIn from "components/auth/SignIn"
 import SignUp from "components/auth/SignUp"
 import { useSearchParams } from "next/navigation"
+import { Suspense } from "react"
 
-const page = () => {
+const SignInUp = () => {
   const params = useSearchParams()
   const method = params.get("method")
 
   return <>{method === "signup" ? <SignUp /> : <SignIn />}</>
+}
+
+const page = () => {
+  return (
+    <Suspense>
+      <SignInUp />
+    </Suspense>
+  )
 }
 
 export default page
