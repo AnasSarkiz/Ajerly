4b8a55fd081d0999fc8197ac59c4d1eba5116d3e
Anas sarkiz <anassoftwaredev@gmail.com>
2025-02-27 16:04:17 +0200

    Implemented UI page fo create-new-pos

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 import { Suspense } from "react"
 import MobileMenu from "./mobile-menu"
 import Search, { SearchSkeleton } from "./search"
-
+import CreatePostLogo from "./createPostLogo"
 const { SITE_NAME } = process.env
 
 export async function Navbar() {
@@ -32,6 +32,7 @@ export async function Navbar() {
               {SITE_NAME}
             </div>
           </Link>
+
           {menu.length ? (
             <ul className="hidden gap-6 text-sm md:flex md:items-center">
               {menu.map((item: Menu) => (
@@ -54,6 +55,7 @@ export async function Navbar() {
           </Suspense>
         </div>
         <div className="flex items-center gap-2 justify-end md:w-1/3">
+          <CreatePostLogo />
           <LoginButton />
           <ThemeSwitcher />
         </div>
